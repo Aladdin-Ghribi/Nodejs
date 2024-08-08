3d284bb7def37bc33028300750237b2858d9cd9b
Alan <alanghribi@gmail.com>
2024-08-08 12:22:11 +0200

    complete status hadnling for all the controll functions

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -22,29 +22,29 @@ const getCourse= async (req,res)=>{
 
 const addCourse= async (req,res)=>{
    const Error=validationResult(req);
-   if(!Error.isEmpty()) return res.status(400).json(Error.array());
+   if(!Error.isEmpty()) return res.status(400).json({status:httpStatus.ERROR,message:"invalid data"});
    const newCourse= new Course(req.body);
    await newCourse.save();
-   res.status(201).json(newCourse);
+   res.status(201).json({status:httpStatus.SUCCSSES,data:{newCourse}});
 };
 
 const editCourse= async (req,res)=>{
    try{
       const courseId= req.params.courseId;
          const updatedCourse = await Course.updateOne({_id: courseId},{$set:{...req.body}});
-         return res.status(200).json(updatedCourse);
+         return res.status(200).json({status:httpStatus.SUCCSSES,data:{course:updatedCourse}});
    }catch(e){
-     return res.status(400).json({error:e});
+     return res.status(400).json({status:httpStatus.ERROR,message:e.message});
    }
 
 };
 
 const deleteCourse= async (req,res)=>{
  try{
-   const r = await Course.deleteOne({_id: req.params.courseId});
-   res.status(200).json({succsses:"True"});
+    await Course.deleteOne({_id: req.params.courseId});
+   res.status(200).json({status:httpStatus.SUCCSSES,data:null});
     }catch(e){
-         return res.status(400).json({msg:"bad input"});
+         return res.status(400).json({status:httpStatus.ERROR,message:e.message});
     }
 
 };
